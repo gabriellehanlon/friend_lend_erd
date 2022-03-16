0447ddda9032698e4577a887dbee0de21929bebe
anonymous anonymous <gabrielle.hanlon@kellogg.northwestern.edu>
2022-03-16 02:55:07 +0000

    Whitelist columns in Style for ActiveAdmin

--- app/admin/styles.rb
+++ app/admin/styles.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Style do
 
+ permit_params :style_name
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
