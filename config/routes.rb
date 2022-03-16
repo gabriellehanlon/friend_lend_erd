7fd0b928aa58c4075f8bf81aedf4f65d75c2a245
anonymous anonymous <gabrielle.hanlon@kellogg.northwestern.edu>
2022-03-16 02:54:58 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "preferences#index"
   resources :saved_items
   resources :lend_transactions
