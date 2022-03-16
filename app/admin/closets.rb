2f69960c53fd0596a745e3d7ea3c4c4534476b83
anonymous anonymous <gabrielle.hanlon@kellogg.northwestern.edu>
2022-03-16 02:55:09 +0000

    Whitelist columns in Closet for ActiveAdmin

--- app/admin/closets.rb
+++ app/admin/closets.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Closet do
 
+ permit_params :user_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
