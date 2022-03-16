fdaf56eda68f158b766eb2bbc10dc7f33247b7d4
anonymous anonymous <gabrielle.hanlon@kellogg.northwestern.edu>
2022-03-16 02:57:57 +0000

    Generate graphiti resources for items

---
+++ app/resources/item_resource.rb
@@ -0,0 +1,18 @@
+class ItemResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :closet_id, :integer
+  attribute :item_name, :string
+  attribute :item_type_id, :integer
+  attribute :style_id, :integer
+  attribute :letter_size_id, :integer
+  attribute :number_size_id, :integer
+  attribute :item_description, :string
+  attribute :photos, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
