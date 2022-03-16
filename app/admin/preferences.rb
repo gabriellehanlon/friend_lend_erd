2f16a9cc460b8769c212b64423bb869df1a28d60
anonymous anonymous <gabrielle.hanlon@kellogg.northwestern.edu>
2022-03-16 02:55:03 +0000

    Whitelist columns in Preference for ActiveAdmin

--- app/admin/preferences.rb
+++ app/admin/preferences.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Preference do
 
+ permit_params :user_id, :number_size_id, :letter_size_id, :style_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
